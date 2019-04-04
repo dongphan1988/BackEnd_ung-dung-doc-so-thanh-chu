055a55512744a074a055d1034487050f76b12673
phandong88 <phantrongdong15488@gmail.com>
2019-04-04 08:18:01 +0700

    doi so thanh chu

---
+++ index1.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<form method="post" action="">
+    <input type="text" name="input"/>
+    <input type="submit"/>
+</form>
+</body>
+</html>
+<?php
+$arr = array("1" => "one", "2" => "two", "3" => "three", "4" => "four", "5" => "five", "6" => "six", "7" => "seven", "8" => "eight", "9" => "night");
+$arr1 = array("2" => "twenty", "3" => "thirty", "4" => "forty", "5" => "fifty", "6" => "sixty", "7" => "seventy", "8" => "eighty", "9" => "ninety");
+$arr3 = array("1" => "eleven", "2" => "twelve", "3" => "thirteen", "4" => "fourteen", "5" => "fifteen", "6" => "sixteen", "7" => "seventeen", "8" => "eighteen", "9" => "nineteen");
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $num = $_POST["input"];
+    if (strlen($num) >= 4) {
+        echo "out of ability";
+    } else if (strlen($num) == 3) {
+        echo $arr[$num[0]] . " hundred and " . $arr1[$num[1]] . " " . $arr[$num[2]];
+    } else if ($num >= 20) {
+        echo $arr1[$num[0]] . " " . $arr[$num[1]];
+    } else if ($num >= 11 && $num < 20) {
+        echo $arr3[$num[1]];
+    }else{
+        echo $arr[$num[0]];
+    }
+}
+?>
